c59a4ee3f17175be307a14c9e0834ebf7c697625
Rich Burridge <richb@src.gnome.org>
2003-03-13 21:16:51 +0000

    Changes for v4.2.67; see the ChangeLog for more details.

--- gcalctool/gtk.c
+++ gcalctool/gtk.c
@@ -808,7 +808,7 @@ create_kframe()
 
     X->kframe = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     g_object_set_data(G_OBJECT(X->kframe), "kframe", X->kframe);
-    gtk_window_set_resizable(GTK_WINDOW(X->kframe), TRUE);
+    gtk_window_set_resizable(GTK_WINDOW(X->kframe), FALSE);
 
     g_signal_connect(G_OBJECT(X->kframe), "delete_event",
                        G_CALLBACK(quit_cb), NULL);
