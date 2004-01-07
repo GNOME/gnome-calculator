788735bf6eb310108228096d5ede41e7b62c230f
Rich Burridge <richb@src.gnome.org>
2004-01-07 01:05:57 +0000

    Changes for v4.3.36; see the ChangeLog and NEWS files for more details.

--- gcalctool/get.c
+++ gcalctool/get.c
@@ -220,8 +220,7 @@ get_str_resource(enum res_type rtype, char *strval)
 
 
 /* Return the thousands separator string. For most locales, this is a 
- * comma. If this is the C locale, nl_langinfo(THOUSEP) returns an empty 
- * string, in which case we return ",".
+ * comma. 
  */
 
 const char *
@@ -234,7 +233,7 @@ get_tsep()
         tsep = g_locale_to_utf8(tsep, -1, NULL, NULL, NULL);
     }
 
-    if (tsep == NULL || tsep[0] == '\0') {
+    if (tsep == NULL) {
         return(",");
     } else {
         return(tsep);
