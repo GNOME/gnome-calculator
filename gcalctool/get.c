eece0209f257fb60a6bb61fd8f71e9de6df42324
Sami Pietilä <sampie@src.gnome.org>
2004-07-15 20:09:58 +0000

    Arithmetic precedence mode is now default.

--- gcalctool/get.c
+++ gcalctool/get.c
@@ -381,6 +381,8 @@ read_resources()    /* Read all possible resources from the database. */
         }
     }
 
+    v->syntax = exprs; // Set expression (arithmetic precedence) mode as default
+
     if (get_str_resource(R_SYNTAX, str)) {
       for (i = 0; i < MAXSYNTAX; i++) {
 	if (EQUAL(str, Rsstr[i])) {
