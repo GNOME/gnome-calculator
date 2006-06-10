1164a2c743c449cab3f1f6ace2b12fac6c2f1b68
Francisco Javier F. Serrador <serrador@cvs.gnome.org>
2006-06-10 20:50:42 +0000

    Updated FSF postal address
    2006-06-10  Francisco Javier F. Serrador  <serrador@cvs.gnome.org>
    
    	* gcalctool/gtk.c: Updated FSF postal address

--- gcalctool/gtk.c
+++ gcalctool/gtk.c
@@ -546,7 +546,7 @@ about_cb(GtkAction *action, gpointer date)
         "GNU General Public License for more details.\n"),
         N_("You should have received a copy of the GNU General Public License\n"
         "along with Gcalctool; if not, write to the Free Software Foundation, Inc.,\n"
-        "59 Temple Place, Suite 330, Boston, MA  02111-1307  USA\n")
+        "51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA\n")
     };
 
     char *license_trans = g_strconcat(_(license[0]), "\n", 
