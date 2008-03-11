8a1c2a4d18607acd7c81847713c30efb27f334ff
Robert Ancell <rancell@src.gnome.org>
2008-03-11 23:57:22 +0000

    Fixed bug #520525 - Clear display sets display to empty not "Ans"
    svn path=/branches/gnome-2-22/; revision=2025

--- gcalctool/functions.c
+++ gcalctool/functions.c
@@ -284,7 +284,7 @@ exp_insert(char *text, int cursor)
 void
 exp_clear()
 {
-    exp_replace("Ans");
+    exp_replace("");
 }
 
 
@@ -292,7 +292,11 @@ int
 usable_num(int MPnum[MP_SIZE])
 {
     struct exprm_state *e = get_state();
-    return ce_parse(e->expression, MPnum);
+    if (e->expression[0] == '\0') {
+        return ce_parse("0", MPnum);
+    } else {
+        return ce_parse(e->expression, MPnum);
+    }
 }
 
 
