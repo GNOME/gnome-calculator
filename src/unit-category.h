3545f125c8b0bed4487846229886e6ff95553909
Robert Ancell <robert.ancell@canonical.com>
2011-03-18 15:06:09 +1100

    Tidy up and standardise copyright headers

--- src/unit-category.h
+++ src/unit-category.h
@@ -1,3 +1,14 @@
+/*
+ * Copyright (C) 1987-2008 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright (C) 2008-2011 Robert Ancell.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version. See http://www.gnu.org/copyleft/gpl.html the full text of the
+ * license.
+ */
+
 #ifndef UNIT_CATEGORY_H
 #define UNIT_CATEGORY_H
 
