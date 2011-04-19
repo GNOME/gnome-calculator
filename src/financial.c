d160767e0da26bfe2c3cdf6ec10f80ae4458473c
Kjartan Maraas <kmaraas@gnome.org>
2011-04-20 09:53:27 +1000

    Mark strings for translation that were missed

--- src/financial.c
+++ src/financial.c
@@ -67,7 +67,7 @@ calc_ddb(MathEquation *equation, MPNumber *t, MPNumber *cost, MPNumber *life, MP
     }
 
     if (len >= 0) {
-        math_equation_set_status (equation, ("Error: the number of periods must be positive"));
+        math_equation_set_status (equation, _("Error: the number of periods must be positive"));
         mp_set_from_integer(0, t);
     }
 }
