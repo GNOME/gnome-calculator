b1d71b1a3bad3e6fc5dba85bc9bde1c005961ae9
Robert Ancell <robert.ancell@canonical.com>
2015-05-30 20:15:55 +1200

    Stop using gnome-common as specified in https://wiki.gnome.org/Projects/GnomeCommon/Migration
    https://bugzilla.gnome.org/show_bug.cgi?id=750135

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,36 @@
 #!/bin/sh
+# Run this to generate all the initial makefiles, etc.
 srcdir=`dirname $0`
-[ -z "$srcdir" ] && srcdir=.
+test -z "$srcdir" && srcdir=.
 
-if [ ! -f "$srcdir/configure.ac" ]; then
-	echo "$srcdir doesn't look like source directory for GNOME Calculator" >&2
-	exit 1
-fi
-
-which gnome-autogen.sh || {
-	echo "You need to install gnome-common from GNOME Git"
-	exit 1
+(test -f $srcdir/configure.ac) || {
+        echo "**Error**: Directory "\`$srcdir\'" does not look like the top-level project directory"
+        exit 1
 }
 
-. gnome-autogen.sh "$@"
+PKG_NAME=`autoconf --trace 'AC_INIT:$1' "$srcdir/configure.ac"`
+
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+        echo "**Warning**: I am going to run \`configure' with no arguments." >&2
+        echo "If you wish to pass any to it, please specify them on the" >&2
+        echo \`$0\'" command line." >&2
+        echo "" >&2
+fi
+
+set -x
+aclocal --install || exit 1
+intltoolize --force --copy --automake || exit 1
+autoreconf --verbose --force --install -Wno-portability || exit 1
+set +x
+
+if [ "$NOCONFIGURE" = "" ]; then
+        set -x
+        $srcdir/configure "$@" || exit 1
+        set +x
+
+        if [ "$1" = "--help" ]; then exit 0 else
+                echo "Now type \`make\' to compile $PKG_NAME" || exit 1
+        fi
+else
+        echo "Skipping configure process."
+fi
